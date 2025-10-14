46ddeb4505e28d36d5bd20c342b00b39a6584848
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2025-10-14 10:57:53 -0600

    Use 'all' (both fc_0_output and various LCL-genotype-input block skips to fusion) option as default for input --> fusion connections.

--- src/eir_auto_gp/multi_task/modelling/run_modelling.py
+++ src/eir_auto_gp/multi_task/modelling/run_modelling.py
@@ -292,7 +292,7 @@ class TrainSingleRun(luigi.Task):
             output_dim=self.modelling_config["output_dim"],
             n_lcl_blocks=n_lcl_blocks,
             use_lcl_block_skips=False,
-            fc0_fusion_skip_scope="first_section",
+            fc0_fusion_skip_scope="all",
         )
 
         injection_params = build_injection_params(
