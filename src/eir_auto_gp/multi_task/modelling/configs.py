6b50e061f29bb47c43d3cb142d8333095b754c6a
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2025-10-14 08:20:02 -0600

    Revert caching to fc_0 output.

--- src/eir_auto_gp/multi_task/modelling/configs.py
+++ src/eir_auto_gp/multi_task/modelling/configs.py
@@ -65,10 +65,10 @@ def get_base_input_genotype_config(
 ) -> dict[str, Any]:
     message_configs = [
         {
-            "name": "first_layer_tensor",
+            "name": "fc_0_output",
             "layer_path": "input_modules.genotype.fc_0",
             "cache_tensor": True,
-            "layer_cache_target": "input",
+            "layer_cache_target": "output",
             "kernel_width_divisible_by": 4,
         }
     ]
@@ -295,7 +295,7 @@ def _get_staggered_cache_names(
     total_layers: int,
     n_lcl_blocks: int,
 ) -> list[str]:
-    cache_names = ["first_layer_tensor"]
+    cache_names = ["fc_0_output"]
 
     if n_lcl_blocks == 0:
         return cache_names
@@ -315,7 +315,7 @@ def _get_output_head_cache_names(
     n_lcl_blocks: int,
     use_lcl_block_skips: bool = False,
 ) -> list[str]:
-    cache_names = ["first_layer_tensor"]
+    cache_names = ["fc_0_output"]
 
     if use_lcl_block_skips and n_lcl_blocks >= 1:
         cache_names.extend(["lcl_block_0_fc_1", "lcl_block_0_fc_2"])
